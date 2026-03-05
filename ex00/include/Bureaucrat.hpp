bca500bc089d8982f248f9bb7246900495a87e81
Keereslame <alexisPhilfavre@icloud.com>
2026-03-05 18:34:58 +0100

    ex00 finish

---
+++ ex00/include/Bureaucrat.hpp
@@ -0,0 +1,45 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+# include <iostream>
+# include <exception>
+
+class Bureaucrat
+{
+	public:
+		//Orthodox canonical form
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& copy);
+		Bureaucrat& operator=(const Bureaucrat& copy);
+		~Bureaucrat();
+
+		//Getter
+		const std::string& getName() const;
+		int	getGrade() const;
+
+		//Change grade
+		void	incrementGrade();
+		void	decrementGrade();
+
+		//Exception
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+	private:
+		const std::string	_name;
+		int					_grade;
+};
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& bureaucrat);
+
+#endif
---
+++ ex00/src/Bureaucrat.cpp
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alexis <alexis@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 13:41:01 by alexis            #+#    #+#             */
+/*   Updated: 2026/03/05 18:34:27 by alexis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <exception>
+#include <string>
+#include "../include/Bureaucrat.hpp"
+
+/******************************************************************************/
+/*                          Orthodox canonical form                           */
+/******************************************************************************/
+
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(150)
+{
+	std::cout << _name << " created." << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	else
+		_grade = grade;
+	std::cout << _name << " is created and have the grade of " << _grade << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& copy) : _name(copy._name), _grade(copy._grade)
+{
+	std::cout << _name << " is copied. He has the grade of " << _grade << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& copy)
+{
+	if (this != &copy)
+		_grade = copy._grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << _name << " deleted." << std::endl;
+}
+
+/******************************************************************************/
+/*                                  Getter                                    */
+/******************************************************************************/
+
+const std::string&	Bureaucrat::getName() const
+{
+	return (_name);
+}
+
+int	Bureaucrat::getGrade() const
+{
+	return (_grade);
+}
+
+/******************************************************************************/
+/*                                  Getter                                    */
+/******************************************************************************/
+
+void Bureaucrat::incrementGrade()
+{
+	if (_grade == 1)
+		throw GradeTooHighException();
+	else
+		_grade--;
+}
+
+void Bureaucrat::decrementGrade()
+{
+	if (_grade == 150)
+		throw GradeTooLowException();
+	else
+		_grade++;
+}
+
+/******************************************************************************/
+/*                               Exception                                    */
+/******************************************************************************/
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade too high!");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade too low!");
+}
+
+/******************************************************************************/
+/*                            Override function                               */
+/******************************************************************************/
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& bureaucrat)
+{
+	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << "." << std::endl;
+	return (out);
+}
